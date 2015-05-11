001f5c6c69cc73f6c9ac60abb740baa77dd09942
Benjamin Vialle <benjamin@vialle.io>
2015-05-11 17:16:56 +0200

    Transformation script

--- Gemfile
+++ Gemfile
@@ -3,3 +3,11 @@ source 'https://rubygems.org'
 
 # Ruby bindings for Selenium/WebDriver
 gem 'selenium-webdriver'
+
+gem 'logger'
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', :platforms => [:ruby_20, :mri_20, :mri_21, :ruby_21, :mri_22, :ruby_22]
+  gem 'debugger', :platforms => [:ruby_19, :mri_19]
+end
--- LRAR.rb
+++ LRAR.rb
@@ -1,16 +1,65 @@
-require "selenium-webdriver"
+#!/usr/bin/ruby
+#
+# Shots screenshots on LaPoste.fr
+#
+# (c) Benjamin Vialle, 2015.
+#
+# 0 ; OK
+# 1 ; WARNING
+# 2 ; CRITICAL
+# 3 ; UNKNOWN
 
-driver = Selenium::WebDriver.for :firefox
+begin
+  # in order to log
+  require 'logger'
+  # in order to parse console arguments
+  require 'getoptlong'
+  # we use selenium to drive Firefox
+  require "selenium-webdriver"
+  # used for DateTime functions
+  require 'time'
+rescue LoadError => e
+  $stderr.puts "Required library not found: '#{e.message}'."
+  exit(2)
+end
 
-driver.navigate.to "http://www.csuivi.courrier.laposte.fr/suivi"
-driver.manage.window.maximize
+OPTS = GetoptLong.new(
+      [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+      [ '--debug', '-d', GetoptLong::OPTIONAL_ARGUMENT ]
+    )
 
-element = driver.find_element(:id, 'masqueRechercheInit')
-element.send_keys "00000000000"
-element.submit
+class LRAR
 
-driver.save_screenshot("./screen.png")
+  LRAR = "2C09912810639"
+  URL = "http://www.csuivi.courrier.laposte.fr/suivi"
 
-sleep 5
+  # Start Logging
+  log = Logger.new(STDOUT)
+  # Set the log level here
+  log.level = Logger::INFO
 
-driver.quit
+
+  driver = Selenium::WebDriver.for :firefox
+
+  begin
+
+    driver.navigate.to "http://www.csuivi.courrier.laposte.fr/suivi"
+    driver.manage.window.maximize
+
+    element = driver.find_element(:id, 'masqueRechercheInit')
+    element.send_keys LRAR
+    element.submit
+
+    driver.save_screenshot("./#{LRAR}-screen.png")
+
+    sleep 5
+
+    driver.quit
+
+    #if Firefox window is closed before end of script
+  rescue Errno::ECONNREFUSED => e
+    $stderr.puts e.message
+    exit(2)
+  end
+
+end
